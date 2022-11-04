6726aafddeca487a83981fe72a7f9da22e5d7074
tonyzl <germanantoniosiete@gmail.com>
2022-11-03 20:25:02 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },{
+    path: '**',
+    redirectTo: '/error'
+  }
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
