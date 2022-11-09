897c179861c09eda8be733ada175a53db81cbb41
tonyzl <germanantoniosiete@gmail.com>
2022-11-08 19:53:40 -0500

    NOV082022

---
+++ src/app/modelos/usuario.model.ts
@@ -0,0 +1,9 @@
+export class UsuarioModel{
+    id?: String;
+    nombre?: String;
+    apellidos?: String;
+    telefono?: String;
+    correo?: String;
+    token?: String;
+    isLoggedIn?: boolean = false;
+  }
--- src/app/modulos/seguridad/login/login.component.ts
+++ src/app/modulos/seguridad/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import * as cryptoJS from 'crypto-js';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -14,11 +18,42 @@ export class LoginComponent implements OnInit {
     clave: ['', [Validators.required]]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private seguridadService: SeguridadService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
-  identificarUsuario(){}
+  identificarUsuario(){
+    let usuario = this.fgValidacion.controls["correo"].value as string;
+    let clave = this.fgValidacion.controls["clave"].value as string;
+    
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+ 
+    this.seguridadService.login(usuario, claveCifrada).subscribe(
+      (data: any) => {
+        this.seguridadService.almacenarSesion(data)
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Bienvenido',
+          showConfirmButton: false,
+          timer: 1500
+        }).then(() =>{
+          this.router.navigate(['/index']);
+        })
+      },
+      (error: any) => {
+        console.log(error)
+        Swal.fire({
+          title: 'Error!',
+          text: 'Datos invalidos',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      });
+  }
 
 }
