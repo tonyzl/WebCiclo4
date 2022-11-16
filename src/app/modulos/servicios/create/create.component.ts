dfaa54bdf6a8329e54d9ced3c91252903ad7cf74
tonyzl <germanantoniosiete@gmail.com>
2022-11-15 19:49:18 -0500

    NOV152022

--- src/app/modulos/servicios/create/create.component.ts
+++ src/app/modulos/servicios/create/create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServicioModel } from 'src/app/modelos/servicio.model';
+import { ServicioService } from 'src/app/servicios/servicio.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-create',
@@ -7,7 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private servicioService: ServicioService,
+    private router: Router) { }
+
+  fgValidacion = this.fb.group({
+      fecha: ['', [Validators.required]],
+      hora_inicio: ['', [Validators.required]],
+      hora_fin: ['', [Validators.required]],
+      placa: ['', [Validators.required, Validators.minLength(6)]],
+      nombre_conductor: ['', [Validators.required]],
+      dinero_recogido: ['', [Validators.required]],
+      ruta: ['', [Validators.required]],
+    });
+    
+  store(){
+      let servicio= new ServicioModel();
+      servicio.fecha = this.fgValidacion.controls["fecha"].value as string;
+      servicio.hora_inicio = this.fgValidacion.controls["hora_inicio"].value as string;
+      servicio.hora_fin = this.fgValidacion.controls["hora_fin"].value as string;
+      servicio.placa = this.fgValidacion.controls["placa"].value as string;
+      servicio.nombre_conductor = this.fgValidacion.controls["nombre_conductor"].value as string;
+      servicio.dinero_recogido = this.fgValidacion.controls["dinero_recogido"].value as string;
+      servicio.ruta = this.fgValidacion.controls["ruta"].value as string;
+   
+      this.servicioService.store(servicio).subscribe((data: ServicioModel)=> {
+        Swal.fire('Creado correctamente!', '', 'success')
+        this.router.navigate(['/servicios/get']);
+      },
+      (error: any) => {
+        console.log(error)
+        alert("Error en el envio");
+      })
+  }
 
   ngOnInit(): void {
   }
